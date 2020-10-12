21948e47d27ca4984fbdea754399c0c5c9313216
bakarfreelancer <64140732+bakarfreelancer@users.noreply.github.com>
2020-10-12 12:54:04 +0500

    updated directory structure

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- BOOTSTRAP LINKS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
-    <!-- CUSTOM CSS LINK -->
-    <link rel="stylesheet" href="./assets/css/style.css">
-
-
-
-    <title>JC Swat</title>
-</head>
-<body>
+<?php
+include './includes/components/header.php';
+?>
 
 <!-- BOOTSTRAP JS -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
