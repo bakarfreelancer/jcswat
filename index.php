d2235565f77bcef3b10d8ca6b176b4ce5ffe9ff3
bakarfreelancer <64140732+bakarfreelancer@users.noreply.github.com>
2020-10-13 15:08:39 +0500

    updated index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- BOOTSTRAP LINKS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- CUSTOM CSS LINK -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <!-- FONTAWESOME -->
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <title>JC Swat</title>
+</head>
+<body>
 <?php
 include './includes/components/header.php';
 include './includes/components/footer.php';
