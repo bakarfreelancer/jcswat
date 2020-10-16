bac8fb88d2b3e3cc58b872e03f25e74d15a08cb3
itzbuNNy <tallookhan1@gmail.com>
2020-10-16 22:25:53 +0500

    updated about page

--- includes/pages/about.inc.php
+++ includes/pages/about.inc.php
@@ -1,4 +1,45 @@
-<!-- About -->
+<!-- about -->
 <div class="spacer"></div>
 <div class="spacer"></div>
-<h1>about page</h1>
+<div class="spacer"></div>
+<div class="container">
+<div>
+    <h1 class='headings'>p.g jahanzeb college, saidu sharif, swat</h1>
+
+</div>
+    <div class="spacer"></div>
+        <div class='row'>
+            <div class='col-md-6'>
+                <div img1>
+                <img src="./assets/images/about.png" alt="about img">
+                    <div class='spacer'></div>
+                        <div class='spacer'></div>
+
+        
+
+    
+                </div>
+
+             </div>
+             <div class='col-md-6'>
+             <h2 class='headings'>history of jahanzeb college</h2>
+             <p>Jahanzeb College is a trailblazer for the rest of the institutions in KPK because of its rich history, exceptional teaching and its vital role in the enlightenment, development, shaping and broadening the spectrum of thinking of its students over the years.</p>
+             </div>
+           
+        <div class="spacer"></div>
+        <div class='col-md-3'>
+            <img src="./assets/images/wali.jpg" alt="wali saib">
+            <div class="spacer"></div>
+
+        </div>
+        <div class='col-md-9'>
+            <h3 class='headings'>wali of swat</h3>
+            <p>
+            His Highness, Miangul Abdul Haq Jahanzeb, the former ruler of Swat state, built a lot of schools in every corner of his domain, including the erection of the building of Jahanzeb College in 1952.
+
+            The buildings of Jahanzeb College can be seen in clusters sprawling on both sides of the road that links Mingora and Saidu Sharif.  </p>
+
+
+        </div>
+</div>
+</div>
--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <!-- CUSTOM CSS LINK -->
     <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/about.css">
     <!-- FONTAWESOME -->
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <!-- JQUERY -->
