6cb39447c637354b8249c260e9697e977df68ae3
itzbuNNy <tallookhan1@gmail.com>
2020-10-17 16:31:24 +0500

    updated about and contact

--- includes/pages/about.inc.php
+++ includes/pages/about.inc.php
@@ -38,25 +38,17 @@
         </div>
         <!-- video column close -->
 
-<div class="container-fluid">
-    <div class="container history">
-        <div class="row">
-        <!-- row 1 open -->
-            <div class='col-md-6'>
-                <!-- video column open -->
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/o4kj82SJdc8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-            <!-- video column close -->
-
-            <!-- history of jc column open -->
-            <div class="col-md-6">
-                <h2 class="">history of jahanzeb college</h2>
-                <p>Jahanzeb College is a trailblazer for the rest of the institutions in KPK because of its rich history, exceptional teaching and its vital role in the enlightenment, development, shaping and broadening the spectrum of thinking of its students over the years.</p>
-            </div>
-            <!-- history of jc column close -->
+        <!-- history of jc column open -->
+        <div class="col-md-6">
+            <h2 class="">history of jahanzeb college</h2>
+            <p>Jahanzeb College is a trailblazer for the rest of the institutions in KPK because of its rich history, exceptional teaching and its vital role in the enlightenment, development, shaping and broadening the spectrum of thinking of its students over the years.</p>
         </div>
-        <!-- row 1 close -->
-        <div class="spacer"></div>
+        <!-- history of jc column close -->  
+
+    </div>
+    <!-- row 1 close -->
+    <div class="spacer"></div>
+        </div>
     </div>
     <div class="container-fluid">
         <div class="spacer"></div>
@@ -70,10 +62,9 @@
             <h3 class=''>wali of swat</h3>
             <p>His Highness, Miangul Abdul Haq Jahanzeb, the former ruler of Swat state, built a lot of schools in every corner of his domain, including the erection of the building of Jahanzeb College in 1952. The buildings of Jahanzeb College can be seen in clusters sprawling on both sides of the road that links Mingora Saidu Sharif.  </p>
         </div>
+    </div>
         <!-- row 2 close -->
         <div class="spacer"></div>
-    </div>
-    <!-- history close -->
 </div>
 <!-- history close -->
 
