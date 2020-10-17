96dea27ef1660207c386b022f4651c573a7983e1
itzbuNNy <tallookhan1@gmail.com>
2020-10-17 16:05:01 +0500

    updated about page

--- includes/pages/about.inc.php
+++ includes/pages/about.inc.php
@@ -2,11 +2,34 @@
 <div class="spacer"></div>
 <div class="spacer"></div>
 <div class="spacer"></div>
-<div class="container-fluid">
+<h1 class="text-center">About jc</h1>
+<div class="spacer"></div>
+<div class='container-fluid'>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 principalImg">
+                <img src="./assets/images/prncpl.jpeg"  alt=" princpl">
+                    <span class="principalName">prof.sayyed mumtaz ali shah</span>
+            </div>
+            <div class="col-md-9">
+                <h3>PRINCIPAL MESSAGE</h3>
+                <p class="principalMessagePara">
+                I feel privileged, elated and honoured to pen down a message for the students of the historical institution of Govt.Posgraduate Jahanzeb College, Swat. Undoubtedly, Govt.Jahanzeb College is a trailblazer for the rest of the institutions in KPK because of its rich history, exceptional teaching and its vital role in the enlightenment, development, shaping and broadening the spectrum of thinking of its students over the years. In fact, many lucky students, who graduated from Jahanzeb College have always gone on to excellent careers, who are efficiently serving the nation and reputable institutions 0f the world in various capacities and earning good name for their beloved country Pakistan. Obviously, the credit for this monumental success goes to the exruler of Swat. Maj.Gen. Miangul Abdul Haq Jahanzeb,who laid the foundation of this towering, awesome and inspiring college in 1952, with the clear vision of steering Swat to a more progressive path of peace, prosperity and development. Though established in 1952 for a few hundred local students, but it’s amazing that the college is in that original shape and is catering to the needs of thousands of students both local and from other parts of the province, which is the recognition of the fact college is growing in stature by the day.
+                </p>
+            </div>
+
+        </div>
+    </div>
+        <div class="spacer"></div>
+</div>
+
+<div class="container-fluid bg-pr">
 <div class="container history">
     <!-- history start -->
-    <h1 class="text-center">About jc</h1>
+  
     <div class="spacer"></div>
+    <div class="spacer"></div>
+
     <div class="row">
     <!-- row 1 open -->
         <div class='col-md-6'>
@@ -28,6 +51,7 @@
         </div>
     </div>
     <div class="container-fluid">
+        <div class="spacer"></div>
 <div class="container waliSwat">
     <div class="row">
         <!-- row2 open -->
@@ -46,3 +70,41 @@
 
 </div>
 <!-- container-fluid close -->
+<!-- slider -->
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active aboutSlider">
+     
+      <div class="carousel-caption d-none d-md-block slidertext">
+        <h5>First slide label</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
+    </div>
+    <div class="carousel-item aboutSlider">
+      
+      <div class="carousel-caption d-none d-md-block slidertext">
+        <h5>Second slide label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+    <div class="carousel-item aboutSlider">
+           <div class="carousel-caption d-none d-md-block slidertext">
+        <h5>Third slide label</h5>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>  
