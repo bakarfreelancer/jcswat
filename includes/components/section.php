146a98d6cc926305d613a9c3f9a9811929490888
itzbuNNy <tallookhan1@gmail.com>
2020-10-17 22:08:20 +0500

    malang

--- includes/components/section.php
+++ includes/components/section.php
@@ -3,14 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/test.css">
     <title>jcswat</title>
 </head>
 <body>
         <section>
+                <div class="container">
+                    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop">
+  abubakar full malang d chata sawaya ma aw click ka
+</button>
 
-
-        <div>
-        </div>
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Malang abubakar</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" style="width:100%" >
+            <div class= "marQuee">
+                <p>abubakr malang dy</p>
+                <p>abubakr pullmalang dy</p>
+                <p>abubakr beshaka malang dy</p>
+                <p>abubakr ashaq darhai malang dy</p>
+                <p>abubakr pull pachas malang dy</p>
+                <p>abubakr sav feesad malang dy</p>
+            </div>
+        
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+                </div>
 
         
         </section>
---
+++ includes/pages/about.inc.php
@@ -0,0 +1,110 @@
+<!-- about -->
+<div class="spacer"></div>
+<div class="spacer"></div>
+<div class="spacer"></div>
+<h1 class="text-center">About jc</h1>
+<div class="spacer"></div>
+<div class='container-fluid'>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 principalImg">
+                <img src="./assets/images/prncpl.jpeg"  alt=" princpl">
+                    <span class="principalName">prof.sayyed mumtaz ali shah</span>
+            </div>
+            <div class="col-md-9">
+                <h3>PRINCIPAL MESSAGE</h3>
+                <p class="principalMessagePara">
+                I feel privileged, elated and honoured to pen down a message for the students of the historical institution of Govt.Posgraduate Jahanzeb College, Swat. Undoubtedly, Govt.Jahanzeb College is a trailblazer for the rest of the institutions in KPK because of its rich history, exceptional teaching and its vital role in the enlightenment, development, shaping and broadening the spectrum of thinking of its students over the years. In fact, many lucky students, who graduated from Jahanzeb College have always gone on to excellent careers, who are efficiently serving the nation and reputable institutions 0f the world in various capacities and earning good name for their beloved country Pakistan. Obviously, the credit for this monumental success goes to the exruler of Swat. Maj.Gen. Miangul Abdul Haq Jahanzeb,who laid the foundation of this towering, awesome and inspiring college in 1952, with the clear vision of steering Swat to a more progressive path of peace, prosperity and development. Though established in 1952 for a few hundred local students, but it’s amazing that the college is in that original shape and is catering to the needs of thousands of students both local and from other parts of the province, which is the recognition of the fact college is growing in stature by the day.
+                </p>
+            </div>
+
+        </div>
+    </div>
+        <div class="spacer"></div>
+</div>
+
+<div class="container-fluid bg-pr">
+<div class="container history">
+    <!-- history start -->
+  
+    <div class="spacer"></div>
+    <div class="spacer"></div>
+
+    <div class="row">
+    <!-- row 1 open -->
+        <div class='col-md-6'>
+            <!-- video column open -->
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/o4kj82SJdc8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <!-- video column close -->
+
+        <!-- history of jc column open -->
+        <div class="col-md-6">
+            <h2 class="">history of jahanzeb college</h2>
+            <p>Jahanzeb College is a trailblazer for the rest of the institutions in KPK because of its rich history, exceptional teaching and its vital role in the enlightenment, development, shaping and broadening the spectrum of thinking of its students over the years.</p>
+        </div>
+        <!-- history of jc column close -->  
+
+    </div>
+    <!-- row 1 close -->
+    <div class="spacer"></div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="spacer"></div>
+<div class="container waliSwat">
+    <div class="row">
+        <!-- row2 open -->
+        <div class="col-md-3">
+            <img src="./assets/images/wali.jpg" alt="wali saib">
+        </div>
+        <div class='col-md-9'>
+            <h3 class=''>wali of swat</h3>
+            <p>His Highness, Miangul Abdul Haq Jahanzeb, the former ruler of Swat state, built a lot of schools in every corner of his domain, including the erection of the building of Jahanzeb College in 1952. The buildings of Jahanzeb College can be seen in clusters sprawling on both sides of the road that links Mingora Saidu Sharif.  </p>
+        </div>
+    </div>
+        <!-- row 2 close -->
+        <div class="spacer"></div>
+</div>
+<!-- history close -->
+
+</div>
+<!-- container-fluid close -->
+<!-- slider -->
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active aboutSlider">
+     
+      <div class="carousel-caption d-none d-md-block slidertext">
+        <h5>First slide label</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
+    </div>
+    <div class="carousel-item aboutSlider">
+      
+      <div class="carousel-caption d-none d-md-block slidertext">
+        <h5>Second slide label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+    <div class="carousel-item aboutSlider">
+           <div class="carousel-caption d-none d-md-block slidertext">
+        <h5>Third slide label</h5>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>  
