c3375040d2923efe5cc1a075f72969497564a3da
bakarfreelancer <64140732+bakarfreelancer@users.noreply.github.com>
2020-10-20 16:01:29 +0500

    updated news button

--- includes/components/header.php
+++ includes/components/header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
   <div class="container">
-  <a class="navbar-brand" href="#"><img src="assets/images/logo_300.png" class="logo" alt="JC Swat"></a>
+  <a class="navbar-brand" href="index.php"><img src="assets/images/logo_300.png" class="logo" alt="JC Swat"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
